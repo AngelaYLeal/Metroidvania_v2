363b28b0d96e8bf81d1d64b8e5b5a227922c5a8e
AngelaYLeal <anyi.adh@gmail.com>
2026-04-24 18:32:11 +0200

    TEXTO-PRUEBA

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,15 @@ document.addEventListener( 'DOMContentLoaded', function () {
         },
     }).mount( window.splide.Extensions );
 });
+
+//LOADER
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loader-wrapper");
+    // Añadimos la clase para ocultarlo con la transición
+    loader.classList.add("loader-hidden");
+
+    // Lo eliminamos del DOM después de la animación para que no moleste
+    loader.addEventListener("transitionend", function() {
+        loader.remove();
+    });
+});
